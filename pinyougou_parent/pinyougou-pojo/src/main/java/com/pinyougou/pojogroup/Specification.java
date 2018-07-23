de6bb216361c06db8aba2ff875804551b297d0b5
UNSWTJY <tanjianyuedu@163.com>
2018-07-24 00:47:22 +0800

    day03_finish

---
+++ pinyougou_parent/pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Specification.java
@@ -0,0 +1,30 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Specification implements Serializable {
+
+    private static final long serialVersionUID = 3795960739462486431L;
+    private TbSpecification specification;
+    private List<TbSpecificationOption> specificationOptionList;
+
+    public TbSpecification getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(TbSpecification specification) {
+        this.specification = specification;
+    }
+
+    public List<TbSpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+}
---
+++ pinyougou_parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SpecificationServiceImpl.java
@@ -0,0 +1,142 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import java.util.List;
+
+import com.pinyougou.mapper.TbSpecificationOptionMapper;
+import com.pinyougou.pojo.TbSpecificationOption;
+import com.pinyougou.pojo.TbSpecificationOptionExample;
+import com.pinyougou.pojogroup.Specification;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbSpecificationMapper;
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationExample;
+import com.pinyougou.pojo.TbSpecificationExample.Criteria;
+import com.pinyougou.sellergoods.service.SpecificationService;
+
+import entity.PageResult;
+
+/**
+ * 服务实现层
+ *
+ * @author Administrator
+ */
+@Service
+public class SpecificationServiceImpl implements SpecificationService {
+
+    @Autowired
+    private TbSpecificationMapper specificationMapper;
+
+    @Autowired
+    private TbSpecificationOptionMapper tbSpecificationOptionMapper;
+
+    /**
+     * 查询全部
+     */
+    @Override
+    public List<TbSpecification> findAll() {
+        return specificationMapper.selectByExample(null);
+    }
+
+    /**
+     * 按分页查询
+     */
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbSpecification> page = (Page<TbSpecification>) specificationMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    /**
+     * 增加
+     */
+    @Override
+    public void add(Specification specification) {
+        specificationMapper.insert(specification.getSpecification());
+        //增加规格列表项
+        for (TbSpecificationOption tbSpecificationOption : specification.getSpecificationOptionList()) {
+            tbSpecificationOption.setSpecId(specification.getSpecification().getId());
+            tbSpecificationOptionMapper.insert(tbSpecificationOption);
+        }
+    }
+
+
+    /**
+     * 修改
+     */
+    @Override
+    public void update(Specification specification) {
+        specificationMapper.updateByPrimaryKey(specification.getSpecification());
+        //更新规格列表项
+        //先删除原有列表项
+        TbSpecificationOptionExample example = new TbSpecificationOptionExample();
+        TbSpecificationOptionExample.Criteria criteria = example.createCriteria();
+        criteria.andSpecIdEqualTo(specification.getSpecification().getId());
+        tbSpecificationOptionMapper.deleteByExample(example);
+        //再添加新的列表项
+        for (TbSpecificationOption tbSpecificationOption : specification.getSpecificationOptionList()) {
+            tbSpecificationOption.setSpecId(specification.getSpecification().getId());
+            tbSpecificationOptionMapper.insert(tbSpecificationOption);
+        }
+
+    }
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public Specification findOne(Long id) {
+        TbSpecification tbSpecification = specificationMapper.selectByPrimaryKey(id);
+        //查询规格列表项
+        TbSpecificationOptionExample example = new TbSpecificationOptionExample();
+        TbSpecificationOptionExample.Criteria criteria = example.createCriteria();
+        criteria.andSpecIdEqualTo(id);
+        List<TbSpecificationOption> optionList = tbSpecificationOptionMapper.selectByExample(example);
+        Specification specification = new Specification();
+        specification.setSpecificationOptionList(optionList);
+        specification.setSpecification(tbSpecification);
+        return specification;
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            specificationMapper.deleteByPrimaryKey(id);
+            //删除规格列表
+            TbSpecificationOptionExample example = new TbSpecificationOptionExample();
+            TbSpecificationOptionExample.Criteria criteria = example.createCriteria();
+            criteria.andSpecIdEqualTo(id);
+            tbSpecificationOptionMapper.deleteByExample(example);
+
+        }
+    }
+
+
+    @Override
+    public PageResult findPage(TbSpecification specification, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+
+        TbSpecificationExample example = new TbSpecificationExample();
+        Criteria criteria = example.createCriteria();
+
+        if (specification != null) {
+            if (specification.getSpecName() != null && specification.getSpecName().length() > 0) {
+                criteria.andSpecNameLike("%" + specification.getSpecName() + "%");
+            }
+
+        }
+
+        Page<TbSpecification> page = (Page<TbSpecification>) specificationMapper.selectByExample(example);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+}
